2c211207ca0283a0d4211a01603dd6096844cbe8
Arne Brasseur <arne@arnebrasseur.net>
2014-09-18 21:47:03 +0200

    Update description and URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chestnut/lein-template "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Leiningen template for a ClojureScript setup with Figwheel, Austin, Om."
+  :url "https://github.com/plexus/chestnut"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
