f942dfaccb7e3c8cdc498a2e750c0e31f251c4cc
Arne Brasseur <arne@arnebrasseur.net>
2014-09-21 21:16:09 +0200

    Bump version to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chestnut/lein-template "0.1.0"
+(defproject chestnut/lein-template "0.2.0"
   :description "A Leiningen template for a ClojureScript setup with Figwheel, Austin, Om."
   :url "https://github.com/plexus/chestnut"
   :license {:name "Eclipse Public License"
