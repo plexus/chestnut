47e1140764e077ff8d6e5b3236d40a2f8d9efb93
David Nolen <david.nolen@gmail.com>
2014-10-10 07:57:11 -0400

    improved dev/prod configuration
    generate seperate source directories for code needed to config dev and
    prod builds. prod builds now simply invoke the app's main entry
    point. dev builds setup figwheel and weasel - figwheel is configured to
    call the main entry point on refresh - Om root is idempotent so this
    just works. This remove the invasive change to the root component the
    previous refreshing strategy had.
    
    Bump to ClojureScript 2371 which fixes Weasel.

--- src/leiningen/new/chestnut/src/cljs/chestnut/core.cljs
+++ src/leiningen/new/chestnut/src/cljs/chestnut/core.cljs
@@ -1,25 +1,15 @@
 (ns {{name}}.core
-  (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [{{name}}.dev :as dev]
-            [cljs.core.async :refer [chan <!]]
-            [om.core :as om :include-macros true]{{{core-cljs-requires}}}))
+  (:require [om.core :as om :include-macros true]
+            {{{core-cljs-requires}}}))
 
 (defonce app-state (atom {:text "Hello Chestnut!"}))
-(defonce re-render-ch (chan))
 
-(dev/setup re-render-ch)
-
-(om/root
-  (fn [app owner]
-    (reify
-      om/IWillMount
-      (will-mount [_]
-        (go (loop []
-              (when (<! re-render-ch)
-                (om/refresh! owner)
-                (recur)))))
-      om/IRender
-      (render [_]
-        (dom/h1 {{#not-om-tools?}} nil{{/not-om-tools?}}(:text app)))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+(defn main []
+  (om/root
+    (fn [app owner]
+      (reify
+        om/IRender
+        (render [_]
+          (dom/h1 {{#not-om-tools?}} nil{{/not-om-tools?}}(:text app)))))
+    app-state
+    {:target (. js/document (getElementById "app"))}))
