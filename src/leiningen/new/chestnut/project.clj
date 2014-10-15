00a55dad6aac19642ffc500c87e734ae5133a701
Olli Piepponen <opieppo@gmail.com>
2014-10-15 23:56:27 +0700

    Added support for cljx for the project.clj file

--- src/leiningen/new/chestnut/project.clj
+++ src/leiningen/new/chestnut/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :source-paths ["src/clj" "src/cljs"]
+  :source-paths ["src/clj" "src/cljs"{{{cljx-source-paths}}}]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371" :scope "provided"]
@@ -23,9 +23,11 @@
 
   :min-lein-version "2.5.0"
 
+  :jar-exclusions [#"\.swp|\.swo|\.DS_Store{{{cljx-extension}}}"]
+
   :uberjar-name "{{name}}.jar"
 
-  :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
+  :cljsbuild {:builds {:app {:source-paths ["src/cljs"{{{cljx-cljsbuild-spath}}}]
                              :compiler {:output-to     "resources/public/js/app.js"
                                         :output-dir    "resources/public/js/out"
                                         :source-map    "resources/public/js/out.js.map"
@@ -36,14 +38,29 @@
 
   :profiles {:dev {:repl-options {:init-ns {{project-ns}}.server
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-                   :plugins [[lein-figwheel "0.1.4-SNAPSHOT"]]
+
+                   :plugins [[lein-figwheel "0.1.4-SNAPSHOT"]{{{project-dev-plugins}}}]
+
                    :figwheel {:http-server-root "public"
                               :port 3449
                               :css-dirs ["resources/public/css"]}
+
                    :env {:is-dev true}
+
+                   {{#cljx-hook?}}
+                   :hooks [cljx.hooks]
+                   {{/cljx-hook?}}
+                   {{#cljx-build?}}
+                   :cljx {:builds [{:source-paths ["src/cljx"]
+                                    :output-path "target/generated/clj"
+                                    :rules :clj}
+                                   {:source-paths ["src/cljx"]
+                                    :output-path "target/generated/cljs"
+                                    :rules :cljs}]}
+                   {{/cljx-build?}}
                    :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]}}}}
 
-             :uberjar {:hooks [leiningen.cljsbuild]
+             :uberjar {:hooks [{{cljx-uberjar-hook}}leiningen.cljsbuild]
                        :env {:production true}
                        :omit-source true
                        :aot :all
