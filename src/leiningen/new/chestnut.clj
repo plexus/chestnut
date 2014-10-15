8737592e05135f7f0c525e2823ceae6dcb5b2a09
Olli Piepponen <opieppo@gmail.com>
2014-10-16 00:23:47 +0700

    Added cljx-nrepl-middleware

--- src/leiningen/new/chestnut.clj
+++ src/leiningen/new/chestnut.clj
@@ -48,6 +48,14 @@
   (cond-> []
           (cljx? opts) (conj cljx-plugin)))
 
+(defn project-nrepl-middleware [opts]
+  (cond-> []
+          (cljx? opts) (conj "cljx.repl-middleware/wrap-cljx")))
+
+(defn middle-wares [list]
+  (fn []
+    (join "" (map #(str "\n" (apply str (repeat 53 " ")) %) list))))
+
 (defn template-data [name opts]
   {:full-name name
    :name (project-name name)
@@ -58,6 +66,7 @@
    :core-cljs-requires (dep-list 12 (core-cljs-requires opts))
    :project-clj-deps (dep-list 17 (project-clj-deps opts))
    :project-dev-plugins (dep-list 29 (project-plugins opts))
+   :nrepl-middleware (middle-wares (project-nrepl-middleware opts))
    :server-command (if (http-kit? opts) "run-server" "run-jetty")
    :compojure-handler (if (site-middleware? opts) "site" "api")
    :not-om-tools? (fn [block] (if (om-tools? opts) "" block))
