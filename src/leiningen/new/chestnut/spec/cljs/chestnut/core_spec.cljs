af8f40ce2eeebe94f94380ea12c6d66313b19470
Edward Wible <aewible@gmail.com>
2014-11-28 09:55:48 -0500

    Delete some cruft

--- src/leiningen/new/chestnut/spec/cljs/chestnut/core_spec.cljs
+++ src/leiningen/new/chestnut/spec/cljs/chestnut/core_spec.cljs
@@ -8,20 +8,3 @@
 (describe "A ClojureScript test"
           (it "fail. Fix it!"
               (should= 0 1)))
-
-;; (println "Hi Mom!")
-
-;; Tutorial Tests:
-
-#_(.log js/console (. js/document (getElementById "app"))
-      ;(dom/getElementById "query")
-      )
-;; Shows the dom element named #query
-
-;; indicates a click
-#_(let [clicks (listen (dom/getElement "search") "click")]
-  (go (while true
-        (.log js/console (<! clicks)))))
-
-;; test the jsonp query
-;; (go (.log js/console (<! (jsonp (query-url "cats")))))
