bdd2b8d0687deaccd7a20db44a63124cb9c488f9
Rory Gibson <rory.gibson@brightnorth.co.uk>
2015-01-30 11:37:58 +0000

    Remove redundant feature flag definition

--- src/leiningen/new/chestnut.clj
+++ src/leiningen/new/chestnut.clj
@@ -109,7 +109,6 @@
 
    ;; testing features
    :speclj?              (fn [block] (if (speclj? opts) (str "\n" block) ""))
-   :cljstest?            (fn [block] (if (not (speclj? opts)) (str "\n" block) ""))
    :test-src-path        (if (speclj? opts) "\"spec/cljs\"" "\"test/cljs\"")
    :test-command-name    (if (speclj? opts) "\"spec\"" "\"test\"")
    :test-command         (if (speclj? opts)
