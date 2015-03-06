14389b7d51fa74d9af6fca8a4a9d3c8ef7999a3d
Arne Brasseur <arne@arnebrasseur.net>
2015-03-06 20:39:31 +0100

    Make sure the snapshot dependency on jnr-process does not cause issues for regular users

--- project.clj
+++ project.clj
@@ -5,8 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
 
-  :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [com.github.jnr/jnr-process "1.0-SNAPSHOT"]
-                 [clj-webdriver "0.7.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]]
 
-  :aliases {"test" ["run" "-m" "chestnut.test.integration"] })
+  :profiles { :test { :dependencies [[com.github.jnr/jnr-process "1.0-SNAPSHOT"]
+                                     [clj-webdriver "0.7.0-SNAPSHOT"]]}}
+
+  :aliases {"test" ["with-profile" "+test" "run" "-m" "chestnut.test.integration"] })
