be15f32c3576ddd0e02c54a648f692cfd30c7dd6
Arne Brasseur <arne@arnebrasseur.net>
2015-12-22 21:09:19 +0100

    Start work on 0.9.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chestnut/lein-template "0.8.2-SNAPSHOT"
+(defproject chestnut/lein-template "0.9.0-SNAPSHOT"
   :description "A Leiningen template for a ClojureScript setup with Figwheel, Austin, Om."
   :url "https://github.com/plexus/chestnut"
   :license {:name "Eclipse Public License"
