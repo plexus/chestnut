02315a23f885627a83f1f9467cce8385b86541f6
Arne Brasseur <arne@arnebrasseur.net>
2015-12-25 20:41:00 +0100

    Initialize a git repo for the generated project

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
-(defproject chestnut/lein-template "0.9.0-SNAPSHOT"
+(defproject chestnut/lein-template "0.9.0"
   :description "A Leiningen template for a ClojureScript setup with Figwheel, Austin, Om."
   :url "https://github.com/plexus/chestnut"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
 
-  :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+  :dependencies [[clj-jgit "0.8.8"]]
 
-  :profiles { :test { :dependencies [[com.github.jnr/jnr-process "1.0-SNAPSHOT"]
+  :profiles { :test { :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                                     [com.github.jnr/jnr-process "1.0-SNAPSHOT"]
                                      [clj-webdriver "0.7.0-SNAPSHOT"]]}}
 
   :aliases {"test" ["with-profile" "+test" "run" "-m" "chestnut.test.integration"] })
