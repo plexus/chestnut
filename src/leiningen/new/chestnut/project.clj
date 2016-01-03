79317e18a50923f76fbda349df800ddc82d6a021
Arne Brasseur <arne@arnebrasseur.net>
2016-01-03 20:15:23 +0100

    Drop browser caching middleware
    In a production setup you would more typically let your static assets be
    served up by a separate server like nginx, and let that one handle
    caching headers.

--- src/leiningen/new/chestnut/project.clj
+++ src/leiningen/new/chestnut/project.clj
@@ -8,7 +8,6 @@
                  [org.clojure/clojurescript "1.7.189" :scope "provided"]
                  [ring "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
-                 [slester/ring-browser-caching "0.1.1"]
                  [bk/ring-gzip "0.1.1"]
                  [compojure "1.4.0"]
                  [org.omcljs/om "1.0.0-alpha28"]
@@ -58,21 +57,22 @@
 
   :auto {"sassc"  {:file-pattern  #"\.(scss)$"}}{{/sass?}}
 
-  :profiles {:dev {:dependencies [[figwheel "0.5.0-2"]
-                                  [figwheel-sidecar "0.5.0-2"]
-                                  [com.cemerick/piggieback "0.2.1"]
-                                  [org.clojure/tools.nrepl "0.2.12"]{{{project-dev-deps}}}]
+  :profiles {:dev
+             {:dependencies [[figwheel "0.5.0-2"]
+                             [figwheel-sidecar "0.5.0-2"]
+                             [com.cemerick/piggieback "0.2.1"]
+                             [org.clojure/tools.nrepl "0.2.12"]{{{project-dev-deps}}}]
 
-                   :plugins [[lein-figwheel "0.5.0-2"]{{{project-dev-plugins}}}]}
+              :plugins [[lein-figwheel "0.5.0-2"]{{{project-dev-plugins}}}]}
 
-             :uberjar {:source-paths ^:replace ["src/clj" "env/prod/clj"]
-                       :hooks [{{{project-uberjar-hooks}}}]
-                       :env {:browser-caching {"text/javascript" 604800
-                                               "text/html" 0}}
-                       :omit-source true
-                       :aot :all
-                       :cljsbuild {:builds {:app
-                                            {:source-paths ^:replace ["src/cljs" "env/prod/cljs"]
-                                             :compiler
-                                             {:optimizations :advanced
-                                              :pretty-print false}}}}}})
+             :uberjar
+             {:source-paths ^:replace ["src/clj"]
+              :hooks [{{{project-uberjar-hooks}}}]
+              :omit-source true
+              :aot :all
+              :cljsbuild {:builds
+                          [{:id "app"
+                            :source-paths ^:replace ["src/cljs"]
+                            :compiler
+                            {:optimizations :advanced
+                             :pretty-print false}}]}}})
