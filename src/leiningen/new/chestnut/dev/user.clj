31eec3a8a235e0930c56f2e721339beaa6db11c2
Arne Brasseur <arne@arnebrasseur.net>
2016-01-03 20:10:38 +0100

    Drop dev/prod directories for a single dev dir
    Before we had development helper functions like (run) and (browser-repl)
    inside the {{project-ns}}.server namespace, and we would start the repl
    with that as the default namespace, so they were available.
    
    In prod mode we don't want to include figwheel though, and there are
    potentially other small differences, so we had a separate source
    directory for dev and prod, so you could implement a namespace twice,
    once for each env.
    
    The newer setup is simpler, we add our convenience functions to `user`,
    and start the repl in `user` the way it does by default. The good thing
    about this is you have the default clojure repl functions like doc and
    source available as well. This dramatically simplifies the directory layout.

---
+++ src/leiningen/new/chestnut/dev/user.clj
@@ -0,0 +1,25 @@
+(ns user
+  (:require [{{project-ns}}.server]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [figwheel-sidecar.repl-api :as figwheel]))
+
+{{#less?}}
+(defn start-less []
+  (future
+    (println "Starting less.")
+    (clojure.java.shell/sh "lein" "less" "auto")))
+{{/less?}}
+{{#sass?}}
+(defn start-sass []
+  (future
+    (println "Starting sass.")
+    (clojure.java.shell/sh "lein" "auto" "sassc" "once")))
+{{/sass?}}
+
+(def http-handler
+  (wrap-reload {{project-ns}}.server/http-handler))
+
+(defn run []
+  (figwheel/start-figwheel!){{less-sass-start}})
+
+(def browser-repl figwheel/cljs-repl)
