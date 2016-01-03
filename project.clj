800586eb926c3b8c1b59c055928cb531b94dd05f
Arne Brasseur <arne@arnebrasseur.net>
2016-01-03 21:03:08 +0100

    Set version to 0.9.0-SNAPSHOT and release

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject chestnut/lein-template "0.9.0"
-  :description "A Leiningen template for a ClojureScript setup with Figwheel, Austin, Om."
+(defproject chestnut/lein-template "0.9.0-SNAPSHOT"
+  :description "A Leiningen template for a ClojureScript setup with Figwheel, Om."
   :url "https://github.com/plexus/chestnut"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
