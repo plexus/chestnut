df9753ef9d89a7d1112ca9549e3069d34db1812c
Arne Brasseur <arne@arnebrasseur.net>
2016-01-11 23:28:25 +0100

    Enable warnings for reflections and unboxed math
    These are good defaults to have so you know when to add type annotations.

--- src/leiningen/new/chestnut/dev/user.clj
+++ src/leiningen/new/chestnut/dev/user.clj
@@ -5,6 +5,12 @@
             [clojure.java.shell]{{/less?}}{{#sass?}}
             [clojure.java.shell]{{/sass?}}))
 
+;; Let Clojure warn you when it needs to reflect on types, or when it does math
+;; on unboxed numbers. In both cases you should add type annotations to prevent
+;; degraded performance.
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
+
 {{#less?}}
 (defn start-less []
   (future
