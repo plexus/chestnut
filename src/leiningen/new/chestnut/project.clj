bdde14f0ce4f2802e37939409706187008670dc3
Arne Brasseur <arne@arnebrasseur.net>
2016-01-11 22:22:06 +0100

    Update Clojurescript 1.7.189 => 1.7.228

--- src/leiningen/new/chestnut/project.clj
+++ src/leiningen/new/chestnut/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.189" :scope "provided"]
+                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
                  [ring "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
                  [bk/ring-gzip "0.1.1"]
