b776d48fd88bcc7c06e9a1c9159910745b809b44
Arne Brasseur <arne@arnebrasseur.net>
2016-01-13 19:44:43 +0100

    Refer to the http-handler by var
    Introducing this extra level of indirections means that when the server
    implementation (e.g. the routes) changes, and ring.reload reloads it,
    figwheel actually uses the new definition.

--- src/leiningen/new/chestnut/dev/user.clj
+++ src/leiningen/new/chestnut/dev/user.clj
@@ -24,7 +24,7 @@
 {{/sass?}}
 
 (def http-handler
-  (wrap-reload {{project-ns}}.server/http-handler))
+  (wrap-reload #'{{project-ns}}.server/http-handler))
 
 (defn run []
   (figwheel/start-figwheel!){{less-sass-start}})
