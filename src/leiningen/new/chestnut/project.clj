f02221e809db4a47356516dc9c0711d64c1dd0cc
Arne Brasseur <arne@arnebrasseur.net>
2016-02-02 06:47:31 +0100

    Bump Figwheel and Environ to the latest versions

--- src/leiningen/new/chestnut/project.clj
+++ src/leiningen/new/chestnut/project.clj
@@ -12,7 +12,7 @@
                  [ring.middleware.logger "0.5.0"]
                  [compojure "1.4.0"]
                  [org.omcljs/om "1.0.0-alpha28"]
-                 [environ "1.0.1"]{{{project-clj-deps}}}]
+                 [environ "1.0.2"]{{{project-clj-deps}}}]
 
   :plugins [[lein-cljsbuild "1.1.1"]
             [lein-environ "1.0.1"]{{{project-plugins}}}]
@@ -94,12 +94,12 @@
   :auto {"sassc" {:file-pattern  #"\.(scss)$"}}{{/sass?}}
 
   :profiles {:dev
-             {:dependencies [[figwheel "0.5.0-4"]
-                             [figwheel-sidecar "0.5.0-4"]
+             {:dependencies [[figwheel "0.5.0-6"]
+                             [figwheel-sidecar "0.5.0-6"]
                              [com.cemerick/piggieback "0.2.1"]
                              [org.clojure/tools.nrepl "0.2.12"]]
 
-              :plugins [[lein-figwheel "0.5.0-2"]
+              :plugins [[lein-figwheel "0.5.0-6"]
                         [lein-doo "0.1.6"]]
 
               :cljsbuild {:builds
