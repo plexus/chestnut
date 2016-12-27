33eecbdaca4f9a170a135a9dce4c2ad64442df64
Arne Brasseur <arne@arnebrasseur.net>
2016-12-27 12:04:29 +0100

    Update dependencies, description, bump to next SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,14 +1,16 @@
-(defproject chestnut/lein-template "0.14.0"
-  :description "A Leiningen template for a ClojureScript setup with Figwheel, Om."
+(defproject chestnut/lein-template "0.15.0-SNAPSHOT"
+  :description "A Leiningen template for a minimal but complete Clojure/ClojureScript setup."
   :url "https://github.com/plexus/chestnut"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :eval-in-leiningen true
 
   :dependencies [[com.github.plexus/clj-jgit "v0.8.9-preview"]
-                 [org.slf4j/slf4j-nop "1.7.19"]]
+                 [org.slf4j/slf4j-nop "1.7.22"]]
 
-  :profiles {:test {:dependencies [[org.clojure/core.async "0.2.374"]
+  :profiles {:test {:dependencies [[org.clojure/core.async "0.2.395"]
                                    [com.github.jnr/jnr-process "1.0-SNAPSHOT"]
                                    [clj-webdriver "0.7.2"]]}}
 
