b5a0e4b9bb8e01f68a9a00bb8641ebf7f1984b57
Jeff Wong <awole20@gmail.com>
2016-12-29 08:42:16 -0800

    routes fixup

--- src/leiningen/new/chestnut/src/clj/chestnut/routes.clj
+++ src/leiningen/new/chestnut/src/clj/chestnut/routes.clj
@@ -1,14 +1,15 @@
 (ns {{project-ns}}.routes
   (:require [clojure.java.io :as io]
-            [compojure.core :refer [ANY GET PUT POST DELETE defroutes]]
+            [compojure.core :as compojure :refer [ANY GET PUT POST DELETE]]
             [compojure.route :refer [resources]]
             [ring.util.response :refer [response]]))
 
-(defroutes routes
-  (GET "/" _
-    (-> "public/index.html"
-        io/resource
-        io/input-stream
-        response
-        (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
-  (resources "/"))
+(defn routes [_]
+  (compojure/routes
+   (GET "/" _
+        (-> "public/index.html"
+            io/resource
+            io/input-stream
+            response
+            (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
+   (resources "/")))
