5abddac0c7b96f4307385c131b1c2b1e896f847d
Arne Brasseur <arne@arnebrasseur.net>
2016-12-29 21:23:07 +0100

    Add a .dir-locals.el for easy use from CIDER
    In particular this makes `cider-jack-in-clojurescript` use Figwheel for its
    REPL, and it starts the app-system so everything "ticks".

---
+++ src/leiningen/new/chestnut/.dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((cider-refresh-before-fn . "reloaded.repl/suspend")
+         (cider-refresh-after-fn  . "reloaded.repl/resume")
+         (cider-cljs-lein-repl    . "(do (user/run) (user/browser-repl))"))))
