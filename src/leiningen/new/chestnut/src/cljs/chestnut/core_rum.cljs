00cbbceb06188eb32f629833bfdd9fae77de6df4
Will Mruzek <mruzekw@gmail.com>
2017-01-29 19:49:37 -0500

    Add rum support

---
+++ src/leiningen/new/chestnut/src/cljs/chestnut/core_rum.cljs
@@ -0,0 +1,11 @@
+(ns asdf.core
+  (:require [rum.core :as rum]))
+
+(enable-console-print!)
+
+(defonce app-state (atom {:text "Hello Chestnut!"}))
+
+(rum/defc greeting < rum/reactive []
+   [:h1 (:text (rum/react app-state))])
+
+(rum/mount (greeting) (. js/document (getElementById "app")))
