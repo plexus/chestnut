0e11ce6632e5e2641abb48d26d3e2562db295944
ponkore <ponkore@gmail.com>
2018-04-14 20:07:11 +0900

    cider-cljs-lein-repl is deprecated.

--- src/leiningen/new/chestnut/.dir-locals.el
+++ src/leiningen/new/chestnut/.dir-locals.el
@@ -1,3 +1,3 @@
 ((nil . ((cider-refresh-before-fn . "reloaded.repl/suspend")
          (cider-refresh-after-fn  . "reloaded.repl/resume")
-         (cider-cljs-lein-repl    . "(do (user/go) (user/cljs-repl))"))))
+         (cider-default-cljs-repl . "(do (user/go) (user/cljs-repl))"))))
