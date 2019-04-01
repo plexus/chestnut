f3db7a18f7ce6de3441add96b0405029c416f92c
Jeff Wong <awole20@gmail.com>
2019-04-01 11:00:33 -0700

    update piggieback to support latest jdk

--- src/leiningen/new/chestnut.clj
+++ src/leiningen/new/chestnut.clj
@@ -39,7 +39,7 @@
 
 (def project-clj-dev-deps '[[figwheel "0.5.18"]
                             [figwheel-sidecar "0.5.18"]
-                            [cider/piggieback "0.3.10"]
+                            [cider/piggieback "0.4.0"]
                             [cider/cider-nrepl "0.18.0"]
                             [lein-doo "0.1.11"]
                             [reloaded.repl "0.2.4"]])
